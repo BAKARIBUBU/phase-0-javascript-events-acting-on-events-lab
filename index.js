8a54fb93fa7e4126418535d96104f4699be8679b
bakari-rhoka-bubu <bakaribubu@gmail.com>
2024-06-22 10:25:29 +0300

    JS acting on event

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left);
+  dodger.style.left = `${left - 10}px`;
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left);
+  dodger.style.left = `${left + 10}px`;
+}
